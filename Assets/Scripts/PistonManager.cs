0c7049fc0b03b19c3a7af22b6fbc7bf48e3f2a52
Len Ichinose <psychonyannyan@gmail.com>
2020-11-30 18:38:00 +0900

    second commit
    Switch scenes

---
+++ Assets/Scripts/PistonManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistonManager : MonoBehaviour
+{
+    //ボタンを押された回数を格納する変数
+    int buttonScore;
+
+    //プッシュオブジェクトを格納する変数
+    public GameObject PushScore;
+
+
+    public void PushButton()
+    {
+        countPushButton();
+    }
+
+    //ボタンを押された回数を計測する関数
+    void countPushButton()
+    {
+        buttonScore++;
+
+        //押した回数を表示
+        PushText.GetCom
+    }
+}
